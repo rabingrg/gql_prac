c2a56d4198bde419e3103ee24a65b931a8a670a4
rabingrg <wrabeeng@gmail.com>
2023-08-16 11:31:14 +0545

    setting up apollo client

--- graphQL/src/main.tsx
+++ graphQL/src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const client = new ApolloClient({
+  uri:"https://countries.trevorblades.com/graphql",
+  cache: new InMemoryCache()
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>
+);
