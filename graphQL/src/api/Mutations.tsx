b2a45bb21a752513fb0d596dd7609a9051e15b8e
rabingrg <wrabeeng@gmail.com>
2023-08-20 01:01:53 +0545

    feat: mutation - game deletion

--- graphQL/src/api/Mutations.tsx
+++ graphQL/src/api/Mutations.tsx
@@ -1,15 +1,31 @@
 import { gql } from "@apollo/client";
 
 export const ADD_GAME = gql`
-mutation AddGame($game: GameInput!) {
-  addGame(game: $game) {
-    message
+  mutation AddGame($game: GameInput!) {
+    addGame(game: $game) {
+      message
+    }
   }
-}`
+`;
 
 export const ADD_AUTHOR = gql`
-mutation AddAuthor($auth: AuthorInput!) {
-  addAuthor(auth: $auth) {
-    message
+  mutation AddAuthor($auth: AuthorInput!) {
+    addAuthor(auth: $auth) {
+      message
+    }
   }
-}`
+`;
+
+export const DEL_GAME = gql`
+  mutation DeleteGame($deleteGameId: ID!) {
+    deleteGame(id: $deleteGameId) {
+      message
+      success
+      updatedGames {
+        id
+        title
+        platform
+      }
+    }
+  }
+`;
--- graphQL/src/components/Games/Games.tsx
+++ graphQL/src/components/Games/Games.tsx
@@ -1,15 +1,34 @@
-import { useQuery } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
 import { GET_GAMES, GET_GAME_BY_ID } from "../../api/Queries";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Game } from "../../types/GameType";
 import { useNavigate } from "react-router-dom";
+import { DEL_GAME } from "../../api/Mutations";
 
 const Games = () => {
   const { data, loading, error } = useQuery(GET_GAMES);
-  const games = data?.games;
+  const [games, setGames] = useState<Game[]>();
+  const [deleteGame] = useMutation(DEL_GAME);
+
+  useEffect(() => {
+    if (data?.games) {
+      setGames(data.games);
+    }
+  }, [data]);
+
   const [gamId, setGamId] = useState<number>();
+  console.log("org games",games);
 
   const navigate = useNavigate();
+
+  const handleDelete = async (gameID: number) => {
+    const { data } = await deleteGame({
+      variables: {
+        deleteGameId: gameID,
+      },
+    });
+    setGames(data?.deleteGame?.updatedGames);
+  };
 
   const {
     data: gameById,
@@ -29,13 +48,21 @@ const Games = () => {
     <div>
       <h1>List of Games</h1>
       <ol>
-        {games.map((game: Game) => (
-          <li key={game.id} onClick={() => setGamId(game.id)}>
-            {game.title}
-          </li>
+        {games?.map((game: Game) => (
+          <>
+            <li key={game.id} onClick={() => setGamId(game.id)}>
+              {game.title}
+            </li>
+            <br />
+            <button onClick={() => handleDelete(Number(game.id))}>
+              Delete
+            </button>
+            <br />
+            <br />
+          </>
         ))}
       </ol>
-      <button onClick={()=>navigate('addGame')}>Go to Add Games</button>
+      <button onClick={() => navigate("addGame")}>Go to Add Games</button>
 
       {gameByIdLoading && <p>gamebyid loading...</p>}
       {gameByIdError && <p>{`Error :${gameByIdError}`}</p>}
